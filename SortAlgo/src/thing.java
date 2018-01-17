fe8b73b80a3ad09bb5ff82fb52e366a1f93b444d
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-01-17 09:17:29 -0500

    a

--- SortAlgo/src/thing.java
+++ SortAlgo/src/thing.java
@@ -4,12 +4,19 @@ public class thing
 
 	public static void main(String[] args) 
 	{
-
+		int [] test = {3,10,4,5,7,6,8,1};
+		System.out.println(bubble(test));
 	}
 
 	
-	public static void bubble() 
+	public static void bubble(int [] test) 
 	{
+		int swaps = 0;
+		while(swaps!=0)
+		{
+		
+	
+		}
 		
 	}
 	
@@ -20,6 +27,14 @@ public class thing
 	
 	public static void insert() 
 	{
+		
+	}
+	
+	public static void swapper(int [] arr, int x, int y) 
+	{
+		int holder = arr[x];
+		arr[x] = arr[y];
+		arr[y] = holder;
 		
 	}
 }
